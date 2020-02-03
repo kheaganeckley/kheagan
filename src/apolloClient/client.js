388cc90367ab6bb0a8bef017e34d53ae55fc01bc
kheaganeckley <kheagan.eckley@gmail.com>
2020-02-03 18:58:29 +0200

    safety

--- src/apolloClient/client.js
+++ src/apolloClient/client.js
@@ -2,17 +2,6 @@ import Client, { InMemoryCache } from 'apollo-boost'
 import fetch from 'isomorphic-fetch'
 
 
-console.log('====================================');
-console.log(process.env.GATSBY_APOLLO_CLIENT);
-console.log('====================================');
-
-
-console.log('====================================');
-console.log(process.env.GATSBY_HASURA_KEY);
-console.log('====================================');
-
-
-
 export default new Client({
   uri: process.env.GATSBY_APOLLO_CLIENT,
   fetch,
